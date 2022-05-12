f2cd93d2e6569d64629fc7f28f5c325e22452a2a
infiduk <shw@coinplug.com>
2022-05-12 09:51:17 +0900

    :pencil: modify constants.js
    local 환경에서 test 하기 위해 constants.organization 을 infiduk 으로 변경

--- src/constants.js
+++ src/constants.js
@@ -1,43 +1,43 @@
 const constants = {
-  organization: 'metadium',
-  repoName: 'meta-web3-abis',
-  addressesSourceFile: 'contracts.json',
+  organization: "infiduk",
+  repoName: "meta-web3-abis",
+  addressesSourceFile: "contracts.json",
   ABIsSources: {
-    Registry: 'Registry.json',
-    Identity: 'Identity.json',
-    IdentityManager: 'IdentityManager.json',
-    AttestationAgencyRegistry: 'AttestationAgencyRegistry.json',
-    TopicRegistry: 'TopicRegistry.json',
-    Achievement: 'Achievement.json',
-    AchievementManager: 'AchievementManager.json',
-    Staking: 'Staking.json',
-    EnvStorage: 'EnvStorageImp.json',
-    BallotStorage: 'BallotStorage.json',
-    Gov: 'Gov.json',
-    GovImp: 'GovImp.json'
+    Registry: "Registry.json",
+    Identity: "Identity.json",
+    IdentityManager: "IdentityManager.json",
+    AttestationAgencyRegistry: "AttestationAgencyRegistry.json",
+    TopicRegistry: "TopicRegistry.json",
+    Achievement: "Achievement.json",
+    AchievementManager: "AchievementManager.json",
+    Staking: "Staking.json",
+    EnvStorage: "EnvStorageImp.json",
+    BallotStorage: "BallotStorage.json",
+    Gov: "Gov.json",
+    GovImp: "GovImp.json",
   },
 
   branch: {
-    MAINNET: 'mainnet',
-    TESTNET: 'testnet'
+    MAINNET: "mainnet",
+    TESTNET: "testnet",
   },
 
   NETWORK: {
-    '11': {
-      NAME: 'MAINNET',
-      RPC: 'https://api.metadium.com/prod',
-      BRANCH: 'mainnet',
+    11: {
+      NAME: "MAINNET",
+      RPC: "https://api.metadium.com/prod",
+      BRANCH: "mainnet",
       TESTNET: false,
-      EXPLORER: 'https://explorer.metadium.com'
+      EXPLORER: "https://explorer.metadium.com",
     },
-    '12': {
-      NAME: 'TESTNET',
-      RPC: 'https://api.metadium.com/dev',
-      BRANCH: 'testnet',
+    12: {
+      NAME: "TESTNET",
+      RPC: "https://api.metadium.com/dev",
+      BRANCH: "testnet",
       TESTNET: true,
-      EXPLORER: 'https://testnetexplorer.metadium.com'
-    }
-  }
-}
+      EXPLORER: "https://testnetexplorer.metadium.com",
+    },
+  },
+};
 
-export { constants }
+export { constants };
